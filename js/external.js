9ef692019330cadbd3634b15bd7f9afaa994fe4d
Elliot Anderton <elliot.j.anderton@gmail.com>
2023-04-14 13:25:17 -0700

    Created functions_js.html and downloaded functions.js

--- js/external.js
+++ js/external.js
@@ -3,9 +3,7 @@ console.log('Hello from External JavaScript');
 
 alert("Welcome to my Website");
 
-var favColor;
-
-favColor = prompt('What is your favorite color?');
+let favColor = prompt('What is your favorite color?');
 alert('Awesome, ' + favColor + ' is my favorite color too!');
 
 let daysLM = Number(prompt('How many days will you rent Little Mermaid?'));
@@ -13,7 +11,7 @@ let daysBB = Number(prompt('How many days will you rent Brother Bear?'));
 let daysHU = Number(prompt('How many days will you rent Hercules?'));
 let price = Number(prompt('What is the price per day?'));
 let total = (daysLM + daysBB + daysHU) * price;
-alert('Your total is ' + total.toFixed(2));
+alert('Your total is $' + total.toFixed(2));
 
 let ar = Number(prompt('What is your rate for Amazon?'));
 let ah = Number(prompt('How many how many hours did you work for Amazon?'));
@@ -22,7 +20,7 @@ let gh = Number(prompt('How many how many hours did you work for Google?'));
 let fr = Number(prompt('What is your rate for Facebook?'));
 let fh = Number(prompt('How many how many hours did you work for Facebook?'));
 let totalPay = ar * ah + gr * gh + fr * fh;
-alert('Your total pay is ' + totalPay.toFixed(2));
+alert('Your total pay is $' + totalPay.toFixed(2));
 
 let full = confirm('Is the class full?');
 let scheddy = confirm('Do you have schedule conflicts?');
@@ -33,7 +31,7 @@ if (!full && !scheddy) {
 }
 
 let numItems = Number(prompt('How many items are you buying'));
-let offerexpired = confirm('Is offer sitll valid?');
+let offerexpired = confirm('Is offer still valid?');
 let prem = confirm('Are you a premium member?');
 let applyOffer = offerexpired && (prem || numItems > 2);
 if (applyOffer) {
