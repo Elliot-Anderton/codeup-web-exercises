a7f7e7473eb293f448940bb5e58850c1adeefd5b
Elliot Anderton <elliot.j.anderton@gmail.com>
2023-04-18 10:54:53 -0700

    Added for_loops.js and loops.html

---
+++ js/for_loops.js
@@ -0,0 +1,37 @@
+
+"use strict"
+
+function showMultiplicationTable(num) {
+	for (let i = 1; i < 11; i++) {
+		console.log(num + " x " + i + " = " + (num * i));
+	}
+}
+
+for (let i = 0; i < 10; i++) {
+	let sumnum = Math.floor(Math.random() * 180 + 20);
+	if (sumnum % 2 === 0) {
+		console.log(sumnum + " is even");
+	} else {
+		console.log(sumnum + " is odd");
+	}
+}
+
+
+
+for (let i = 1, k = i.toString(); i < 10; i++) {
+	for (let j = 1; j < i; j++) {
+		k += i.toString()
+	}
+	console.log(k);
+	k = (i + 1).toString();
+}
+
+for (let i = 100; i > 0; i -= 5) {
+	console.log(i);
+}
+
+
+
+
+
+
