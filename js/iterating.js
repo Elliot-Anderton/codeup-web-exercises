019e1628caa27e56f77b5390be1ee627570e4672
Elliot Anderton <elliot.j.anderton@gmail.com>
2023-04-24 12:28:19 -0700

    Finished iterating arrays exercises. Added manipulating arrays exercises.

--- js/iterating.js
+++ js/iterating.js
@@ -6,29 +6,42 @@
      * Create an array of 4 people's names and store it in a variable called
      * 'names'.
      */
+    let names = ['Billy', 'Bob', 'Jane', 'Jill']
 
     /**
      * TODO:
      * Create a log statement that will log the number of elements in the names
      * array.
      */
+    console.log(names.length);
 
     /**
      * TODO:
      * Create log statements that will print each of the names individually by
      * accessing each element's index.
      */
+    console.log(names[0]);
+    console.log(names[1]);
+    console.log(names[2]);
+    console.log(names[3]);
 
     /**
      * TODO:
      * Write some code that uses a for loop to log every item in the names
      * array.
      */
+    for (let i = 0; i < names.length; i++) {
+        console.log(names[i]);
+    }
 
     /**
      * TODO:
      * Refactor your above code to use a `forEach` loop
      */
+    names.forEach(function (name) {
+        console.log(name);
+    });
+
 
     /**
      * TODO:
@@ -43,5 +56,16 @@
      *  > second([1, 2, 3, 4, 5]) // returns 2
      *  > last([1, 2, 3, 4, 5]) // return 5
      */
+    function firstelemnt(somearray) {
+        return somearray[0]
+    }
+
+    function secondelemnt(somearray) {
+        return somearray[1]
+    }
+
+    function lastelemnt(somearray) {
+        return somearray[somearray.length - 1]
+    }
 
 })();
