69a94389b9a3a5c97375f0c6cbbab2bbaee2c758
Elliot Anderton <elliot.j.anderton@gmail.com>
2023-04-24 12:28:37 -0700

    Practicing writing functions

--- js/function_drills.js
+++ js/function_drills.js
@@ -73,32 +73,126 @@ function isTruthy(input) {
 }
 // Make a function named isFalsy(input), remember that values other than false behave like false
 function isFalsy(input) {
-	return 
+	return input != true;
 }
+
 // Make a function named isVowel(letter)
+function isVowel(letter) {
+	return ['a', 'e', 'i', 'o', 'u'].indexOf(letter.toLowerCase()) !== -1;
+}
+
 // Make a function named isConsonant(letter)
+function isConsonant(letter) {
+	return ['a', 'e', 'i', 'o', 'u'].indexOf(letter.toLowerCase()) === -1;
+}
+
 // Make a function named isCapital(letter)
+function isCapital(letter) {
+	return letter === letter.toUpperCase();
+}
+
 // Make a function named isLowerCase(letter)
+function isLowercase(letter) {
+	return letter === letter.toLowerCase();
+}
+
 // Make a function named hasLowerCase(string) that returns if a string has any lower cased letter
+function hasLowerCase(string) {
+	return !isCapital(string);
+}
 // Make a function named isSpace(letter) that returns if a character is a space character
+function isSpace(letter) {
+	return letter === ' ';
+}
+
 // Make a function named isZero(number)
+function isZero(number) {
+	return number === 0;
+}
+
 // Make a function named notZero(input) that returns true if the input is not zero
+function notZero(input) {
+	return !isZero(input);
+}
+
 // Write a function named lowerCase(string)
+function lowerCase(string){
+	return string.toLowerCase();
+}
+
 // Write a function named double(n) that returns a number times two
+function double(n) {
+	return n * 2;
+}
+
 // Write a function named triple(n) that returns a number times 3
+function triple (n) {
+	return n * 3;
+}
+
 // Write a function named quadruple(n) that returns a number times 4
+function quadruple(n) {
+	return n * 4;
+}
+
 // Write a function named half(n) that returns 1/2 of the provided input
+function half(n) {
+	return n / 2;
+}
+
 // Write a function named subtract(a, b) that returns a minus b
+function subtract(a, b) {
+	return a - b;
+}
+
 // Write a function named multiply(a, b) that returns the product of a times b
+function multiply (a, b) {
+	return a * b;
+}
+
 // Write a function named divide(a, b) that returns a divided by b
+function divide(a, b) {
+	return a / b;
+}
+
 // Write a function named remainder(a, b) that returns the remainder after dividing a by b
+function remainders(a, b) {
+	return a % b;
+}
+
 // Make a function named modulo(a, b) that returns the returns the remainder after dividing a by b
+function modulo(a, b) {
+	return a % b;
+}
 // Write a function named cube(n) that returns n * n * n
+function cube(n) {
+	return Math.pow(n, 3)
+}
+
 // Write a function named squareRoot(n) that returns the square root of the input
+function squareRoot(n) {
+	return Math.pow(n, 1/2);
+}
+
 // Write a function named cubeRoot(n) that returns the cube root of the input
+function cubeRoot(n) {
+	return Math.pow(n, 1/3);
+}
+
 // Write a function named invertSign(number) that returns a negative version of a postive number, a positve version of negative, and false for all else.
+function invertSign(number) {
+	return number * -1;
+}
+
 // Write a function named degreesToRadians(number)
+function degreesToRadians(number) {
+	return number * (Math.PI / 180);
+}
+
 // Write a function named radiansToDegrees(number)
+function radiansToDegrees(number) {
+
+}
 // Make a function named isBlank(input) that determines if a given input is spaces, newline characters, or tabs.
 // 	Make a function named trim(string) that removes empty spaces before and after the input.
 // 	Make a function named areEqual(input1, input2) that returns if both inputs have the same value
@@ -147,3 +241,86 @@ function getLogin() {
 
 	willLoginUser(username, password, oldnuff, isadmin);
 }
+// Write a function named remainder  that will accept two arguments and return the remainder
+// of a division between the two values (the first divided by the second). If either argument is not a
+// number, the function should return false .
+
+
+// 	Consider the following test cases:
+// 	remainder(21,7) // returns 0
+// remainder(-34/3) // returns -1
+// remainder(144, -11) // returns 1
+// remainder(-14, "2") // returns false
+// remainder("36", 9) // returns false
+// remainder("44", "-3") // returns false
+// remainder("Jeff", "Geoff") // returns false
+// remainder(false, true) // returns false
+// remainder() // returns false
+
+
+// Write a function named containsAce  that takes in an argument and returns true  if the
+// 	argument passed is a string that contains the sequence of characters "ace"  somewhere
+// within the string. This check should be case insensitive. The function should return false if the
+// 	argument is not a string, or if it doesn't contain "ace"  somewhere in the string.
+// Consider the following test cases:
+// containsAce("place") // returns true
+// containsAce("ACE Hardware") // returns true
+// containsAce("PaLaCe!1!") // returns true
+// containsAce("camel") // returns false
+// containsAce("CACHE") // returns false
+// containsAce("phase 1") // returns false
+// containsAce("bass") // returns false
+// containsAce(["a","c","e"]) // returns false
+// containsAce(135) // returns false
+// containsAce(true) // returns false
+// containsAce() // returns false
+
+
+// Write a function named divisibleBy5  that accepts an argument returns true  is the
+// argument a number that is evenly divisible by 5. If the argument provided is either not a number,
+// 	or is not evenly divisibile by 5, the function should return false .
+// 	Consider the following test cases:
+// 	divisibleBy5(255) // returns true
+// divisibleBy5(-100) // returns true
+// divisibleBy5(5) // returns true
+// divisibleBy5(0) // returns true
+// divisibleBy5("80") // returns false
+// divisibleBy5(6) // returns false
+// divisibleBy5(-99) // returns false
+// divisibleBy5(false) // returns false
+// divisibleBy5([5.10,15]) // returns false
+// divisibleBy5() // returns false
+
+
+// Write a function named averageThreeNumbers  that accepts three arguments, and returns
+// the average (mean) of the three arguments if all three arguments are numbers. If any/all of the
+// three arguments are not a number, or if there are fewer than three arguments, the function
+// should return false .
+// 	Consider the following test cases:
+// averageThreeNumbers(10,5,15) // returns 10
+// averageThreeNumbers(19,44,-8) // returns 18.333333333333332
+// averageTheNumbers("two", 3, 1) // returns false
+// averageThreeNumbers(2, "3", 1) // returns false
+// averageThreeNumbers(6,7,"8") // returns false
+// averageThreeNumbers("duck", "duck", "goose") // returns false
+// averageThreeNumbers(true, false, true) // returns false
+// averageThreeNumbers(22,24) // returns false
+// averageThreeNumbers() // returns false
+
+
+// Write a function named stringsAreSameLength  that accepts two arguments and returns
+// true  if both arguments are strings of the same length. If either argument is not a string, has
+// fewer than two arguments, or if the two strings are of different lengths, the function should return
+// false .
+// 	Consider the following test cases:
+// 	stringsAreSameLength("duck", "code") // returns true
+// stringsAreSameLength("Buick", "CAMEL") // returns true
+// stringsAreSameLength("ERROR_404", "file-miss") // returns true
+// stringsAreSameLength("ERROR_CODE", "1234567890") // returns true
+// stringsAreSameLength("Oops", 1234) // returns false
+// stringsAreSameLength(22, "huh...") // returns false
+// stringsAreSameLength("Camel", "Case") // returns false
+// stringsAreSameLength(81,34) // returns false
+// stringsAreSameLength("ace", [1,2,3]) // returns false
+// stringsAreSameLength("fisherman") // returns false
+// stringsAreSameLength() // returns false
