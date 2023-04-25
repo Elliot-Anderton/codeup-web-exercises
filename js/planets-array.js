01667c497a4f64ebf65c162021990379f2145b97
Elliot Anderton <elliot.j.anderton@gmail.com>
2023-04-25 07:40:21 -0700

    Completed Manipulating Arrays exercises.

--- js/planets-array.js
+++ js/planets-array.js
@@ -19,22 +19,37 @@
      */
 
     console.log('Adding "The Sun" to the beginning of the planets array.');
+    planets.unshift('The Sun')
     console.log(planets);
 
     console.log('Adding "Pluto" to the end of the planets array.');
+    planets.push('Pluto');
     console.log(planets);
 
     console.log('Removing "The Sun" from the beginning of the planets array.');
+    planets.shift();
     console.log(planets);
 
     console.log('Removing "Pluto" from the end of the planets array.');
+    planets.pop();
     console.log(planets);
 
     console.log('Finding and logging the index of "Earth" in the planets array.');
+    console.log(planets.indexOf('Earth'));
 
     console.log("Reversing the order of the planets array.");
+    planets.reverse();
     console.log(planets);
 
     console.log("Sorting the planets array.");
+    planets.sort();
     console.log(planets);
+
+    // experimenting with splice.
+    // let cheeses = ['cheddar', 'swiss', 'craft american singles'];
+    // let notCheese = cheeses.splice(cheeses.indexOf('craft american singles', 1));
+    // console.log(notCheese);
+    // cheeses.splice(cheeses.length, 0, ...notCheese)
+    // console.log(cheeses);
+
 })();
