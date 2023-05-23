e1950f83c0ea6d47c1358c6542cb9e9115ff1406
Elliot Anderton <elliot.j.anderton@gmail.com>
2023-05-23 12:21:36 -0700

    Completed exercise for jquery mouse events

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,7 +1,26 @@
 $(function () {
 	"use strict";
 
-	$('li').css('font-size', `20px`)
-	$(`p, li, h1`).css(`background-color`, `yellow`)
+	// $('li').css('font-size', `20px`)
+	// $(`p, li, h1`).css(`background-color`, `yellow`)
+	// let div = $(`div`)
+	// div.css(`color`, `brown`)
+
+	$(`h1`).click(function (){
+		$(`h1`).css(`background-color`, `green`);
+	})
+
+	$(`p`).dblclick(function (){
+		$(`p`).css(`font-size`, `18px`);
+	})
+
+	$(`li`).hover(
+		function (){
+		$(`li`).css(`color`, `red`);
+		},
+		function (){
+			$(`li`).css(`color`, `black`);
+		}
+		);
 
 });
