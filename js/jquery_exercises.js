ad214f6c4e89586912235c0f020b633fb8203d07
Elliot Anderton <elliot.j.anderton@gmail.com>
2023-05-23 08:58:15 -0700

    Completed exercise for jquery multiple selectors

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,20 +1,7 @@
 $(function () {
 	"use strict";
 
-	$('li').css('font-size', `20px`).css(`background-color`, `yellow`);
-	$(`p`).css(`background-color`, `yellow`)
-	$(`h1`).css(`background-color`, `yellow`)
-
-	let contents = $('h1').html();
-	alert(contents)
-	//
-	// Remove your custom jQuery code from previous exercises.
-	//
-	// 	Using jQuery, set the font-size of all li elements to 20px.
-	//
-	// 	Craft selectors that highlight all the h1, p, and li elements.
-	//
-	// 	Create a jQuery statement to alert the contents of your h1 element(s).
-
+	$('li').css('font-size', `20px`)
+	$(`p, li, h1`).css(`background-color`, `yellow`)
 
 });
