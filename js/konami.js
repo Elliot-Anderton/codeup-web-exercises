f7d72a3114c6ac3c6adb5af5f1601cfdfda57fd6
Elliot Anderton <elliot.j.anderton@gmail.com>
2023-05-24 08:33:27 -0700

    Continued working on konami code, added confetti.

--- js/konami.js
+++ js/konami.js
@@ -1,21 +1,20 @@
 $(function () {
 	"use strict"
 
-	let code = ``;
-	let konamiCode = `arrowuparrowuparrowdownarrowdownarrowleftarrowrightarrowleftarrowrightbaenter`
-
 	let body = $(`body`)
 	let hide = $(`.hide`)
 
+	let code = [];
+	let konamiCode = [`arrowup`, `arrowup`, `arrowdown`, `arrowdown`, `arrowleft`, `arrowright`, `arrowleft`, `arrowright`, `b`, `a`, `enter`]
+
 	body.on(`keydown`, function (event) {
-		if(code === `` || konamiCode.includes(code.toLowerCase())) {
-			code += event.key
-			console.log(code)
-			if (konamiCode === code.toLowerCase()) {
+		code.push(event.key.toLowerCase())
+		if(konamiCode.join(` `).includes(code.join(` `))) {
+			if (konamiCode.join(` `) === code.join(` `)) {
 				partyTime();
 			}
 		} else {
-			code = ``;
+			code = [];
 		}
 	})
 
@@ -23,6 +22,22 @@ $(function () {
 		body.addClass(`blink`);
 		hide.addClass(`yay`);
 		hide.addClass(`blink2`);
+		makeConfetti();
+	}
+
+	function makeConfetti() {
+		let html = ``
+		for (let i = 0; i < 50; i++) {
+
+			if (i % 2 === 0) {
+				html += `<div class="snow second"></div>`
+			} else if (i % 3 === 0) {
+					html += `<div class="snow third"></div>`
+			} else {
+					html += `<div class="snow"></div>`
+			}
+		$(`#confetti`).html(html)
+		}
 	}
 
 })
