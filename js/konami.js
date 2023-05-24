68c680ded27922444f8a6b8cbdfedeaa76cf8c1a
Elliot Anderton <elliot.j.anderton@gmail.com>
2023-05-23 18:30:27 -0700

    Completed Konami code project

--- js/konami.js
+++ js/konami.js
@@ -1,9 +1,28 @@
 $(function () {
 	"use strict"
 
-	$(`body`).on(`keyup`, function (event) {
-		let code = event.key
-		console.log(code)
+	let code = ``;
+	let konamiCode = `arrowuparrowuparrowdownarrowdownarrowleftarrowrightarrowleftarrowrightbaenter`
+
+	let body = $(`body`)
+	let hide = $(`.hide`)
+
+	body.on(`keydown`, function (event) {
+		if(code === `` || konamiCode.includes(code.toLowerCase())) {
+			code += event.key
+			console.log(code)
+			if (konamiCode === code.toLowerCase()) {
+				partyTime();
+			}
+		} else {
+			code = ``;
+		}
 	})
+
+	function partyTime() {
+		body.addClass(`blink`);
+		hide.addClass(`yay`);
+		hide.addClass(`blink2`);
+	}
 
 })
